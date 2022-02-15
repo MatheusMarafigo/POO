e3c7551b00591c4c3f78a1a4a51c9f4dcdb9878b
MatheusMarafigo <99262689+MatheusMarafigo@users.noreply.github.com>
2022-02-15 15:42:59 -0300

    Add files via upload

---
+++ POO/ExemploPOO/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+namespace ExemploPOO.Models
+{
+    public class Pessoa
+    {
+        public string? nome {get; set; }
+        
+        public int Idade {get; set; }
+
+        public string Documento {get; set;}
+
+        public void Aprensetar()
+        {
+            Console.WriteLine($"Olá meu é {nome} e tenho {Idade} anos e o N° {Documento} ");
+        }
+    }
+}
