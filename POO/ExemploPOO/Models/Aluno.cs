6178eb8bb891d9f1fea279626089707869a5e10b
MatheusMarafigo <99262689+MatheusMarafigo@users.noreply.github.com>
2022-02-17 14:16:58 -0300

    Delete POO directory

--- POO/ExemploPOO/Models/Aluno.cs
+++
@@ -1,7 +0,0 @@
-namespace ExemploPOO.Models
-{
-    public class Aluno : Pessoa 
-    {
-        public int Nota { get; set; }
-    } 
-}
--- POO/ExemploPOO/Models/Professor.cs
+++
@@ -1,7 +0,0 @@
-namespace ExemploPOO.Models
-{
-    public class Professor : Pessoa
-    {
-        public double Salario { get; set; }
-    }
-}
